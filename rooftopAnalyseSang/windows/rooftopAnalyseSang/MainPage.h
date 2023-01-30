890db97f07fd3b3c2be59339620095f54e3b798d
Medenos <masfervic@hotmail.com>
2023-01-30 15:52:32 -0500

    sussy baka

---
+++ rooftopAnalyseSang/windows/rooftopAnalyseSang/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rooftopAnalyseSang::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rooftopAnalyseSang::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
