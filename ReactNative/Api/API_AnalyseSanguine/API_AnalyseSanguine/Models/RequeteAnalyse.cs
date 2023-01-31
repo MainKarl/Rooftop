97488c9de106e23d8f2c3c84f1c1197475d8a599
MAubin <mazacra12345@gmail.com>
2023-01-31 15:45:21 -0500

    Creation des modèles de l'API

---
+++ ReactNative/Api/API_AnalyseSanguine/API_AnalyseSanguine/Models/RequeteAnalyse.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_AnalyseSanguine.Models
+{
+    public class RequeteAnalyse
+    {
+        [Key]
+        [Required]
+        public int IdRequete { get; set; }
+        [Required]
+        public Guid CodeAcces { get; set; }
+        [Required]
+        public DateTime DateEchantillon { get; set; } = DateTime.Now;
+        [Required]
+        public string AnalyseDemande { get; set; }
+        [Required]
+        public string NomTechnicien { get; set; }
+
+
+        //Lien
+        [Required]
+        public Dossier Dossier { get; set; }
+        [Required]
+        public Medecin Medecin { get; set; }
+        public List<ResultatAnalyse> LstResultats { get; set; }
+        public List<TypeAnalyse> LstTypeAnalyse { get; set; }
+
+    }
+}
