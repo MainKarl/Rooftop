c4c6b5a9cd02815bd3ec84abce28bd5ee16bd93d
Louis Garceau <2076183@etu.cegepsth.qc.ca>
2023-02-01 20:59:29 -0500

    Ajout du controller de type de valeur et de type d'analyse

---
+++ ReactNative/Api/API_AnalyseSanguine/API_AnalyseSanguine/Controllers/TypeAnalyseController.cs
@@ -0,0 +1,67 @@
+﻿using API_AnalyseSanguine.Context.Data;
+using API_AnalyseSanguine.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_AnalyseSanguine.Controllers
+{
+    [ApiController]
+    [Route("api/typeanalyse")]
+    [Produces("application/json")]
+    public class TypeAnalyseController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TypeAnalyseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TypeAnalyse>> GetAllTypeAnalyse()
+        {
+            try
+            {
+                var item = _context.TypeAnalyses.ToList();
+                return Ok(item);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TypeAnalyse> GetTypeAnalyse(long id)
+        {
+            try
+            {
+                var item = _context.TypeAnalyses.Where(a => a.IdTypeAnalyse == id).FirstOrDefault();
+
+                if (item == null)
+                    return NotFound();
+
+                return Ok(item);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+
+        [HttpPost("create")]
+        public ActionResult CreateTypeAnalyse(TypeAnalyse typeAnalyse)
+        {
+            try
+            {
+                _context.TypeAnalyses.Add(typeAnalyse);
+                _context.SaveChanges();
+
+                return Ok(typeAnalyse);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+    }
+}
---
+++ ReactNative/Api/API_AnalyseSanguine/API_AnalyseSanguine/Controllers/TypeValeurController.cs
@@ -0,0 +1,67 @@
+﻿using API_AnalyseSanguine.Context.Data;
+using API_AnalyseSanguine.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_AnalyseSanguine.Controllers
+{
+    [ApiController]
+    [Route("api/typevaleur")]
+    [Produces("application/json")]
+    public class TypeValeurController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TypeValeurController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TypeValeur>> GetAllTypeValeur()
+        {
+            try
+            {
+                var item = _context.TypeValeurs.ToList();
+                return Ok(item);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TypeValeur> GetTypeValeur(long id)
+        {
+            try
+            {
+                var item = _context.TypeValeurs.Where(a => a.IdTypeValeur == id).FirstOrDefault();
+
+                if (item == null)
+                    return NotFound();
+
+                return Ok(item);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+
+        [HttpPost("create")]
+        public ActionResult CreateTypeValeur(TypeValeur typeValeur)
+        {
+            try
+            {
+                _context.TypeValeurs.Add(typeValeur);
+                _context.SaveChanges();
+
+                return Ok(typeValeur);
+            }
+            catch
+            {
+                return Problem();
+            }
+        }
+    }
+}
